0ce702a6334c4098d4acd7c88a6ad41b7b8aded9
adeshp16 <adeshp16@hawk.iit.edu>
2017-03-28 23:09:59 -0400

    adding the updated hibernet test class

--- src/com/ashish/hibernate/HibernateTest.java
+++ src/com/ashish/hibernate/HibernateTest.java
@@ -7,23 +7,44 @@ import org.hibernate.cfg.Configuration;
 import com.ashish.dto.UserDetails;
 
 public class HibernateTest {
+	
+	//private static SessionFactory sessionFactory=null
 
 	public static void main(String[] args) {
 
 		UserDetails details= new UserDetails();
 		
-		details.setUserId(6);
-		details.setUserName("six");
+		details.setUserId(32);
+		details.setUserName("JavaBrains");
 	
 		
+		HibernateTest test=new HibernateTest();
+		test.setUserDetails(details);
 		
-		SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory();
+	}
+	
+	public void setUserDetails(UserDetails details)
+	{
+		HibernateTest test=new HibernateTest();
+		SessionFactory sessionFactory= test.getSessionFactory();
 		Session session = sessionFactory.openSession();
 		
 		session.beginTransaction();
 		session.save(details);
 		session.getTransaction().commit();
 		
+		sessionFactory.close();
+	}
+	
+	public SessionFactory getSessionFactory()
+	{
+		SessionFactory sessionFactory=null;
+		if(sessionFactory==null)
+		{
+		 sessionFactory= new Configuration().configure().buildSessionFactory();
+		}
+		
+		return sessionFactory;
 	}
 
 }
